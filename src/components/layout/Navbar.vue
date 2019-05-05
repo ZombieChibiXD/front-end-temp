1cb36d1968ef8a1a253461c3e544cdd83eab7090
ZombieChibiXD <chibisd10@gmail.com>
2019-05-06 07:17:15 +0800

    Fixed Navbar Center

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,38 +1,35 @@
 <template>
     <div>
-    <b-navbar toggleable="lg" type="dark" class="navbars">
-        <b-navbar-brand :to="{name:'Home'}">Chibi's Website</b-navbar-brand>
+    <b-navbar toggleable="lg" type="dark" class="navbars justify-content-center">
+        <b-navbar-brand :to="{name:'Home'}" class="d-flex justify-content-start mr-auto">Chibi's Website</b-navbar-brand>
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-        <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="mx-auto">
-            <b-nav-item :to="{ name: 'Tag',params: { tagname: 'new' } }" active-class='active disabled'>Newest</b-nav-item>
-            <b-nav-item :to="{ name: 'Tag',params: { tagname: 'popular' } }" active-class='active disabled'>Popular</b-nav-item>
-            <b-nav-item :to="{ name: 'Tag',params: { tagname: 'entertaiment' } }" active-class='active disabled'>Entertaiment</b-nav-item>
-            <b-nav-item :to="{ name: 'Tag',params: { tagname: 'sports' } }" active-class='active disabled'>Sports</b-nav-item>
-            <b-nav-item :to="{ name: 'Tag',params: { tagname: 'lifestyle' } }" active-class='active disabled'>Lifestyle</b-nav-item>
-            <b-nav-item :to="{ name:'About' } " active-class="active disabled">About</b-nav-item>
-          <b-nav-item :to="{ name: 'CreateArtikel' }" active-class='active disabled'>Insert</b-nav-item>
-        </b-navbar-nav>
+        <b-collapse id="nav-collapse" class="w-100" is-nav>
+            <b-navbar-nav class="w-75 justify-content-center mx-auto">
+                <b-nav-item :to="{ name: 'Tag',params: { tagname: 'new' } }" active-class='active disabled'>Newest</b-nav-item>
+                <b-nav-item :to="{ name: 'Tag',params: { tagname: 'popular' } }" active-class='active disabled'>Popular</b-nav-item>
+                <b-nav-item :to="{ name: 'Tag',params: { tagname: 'entertaiment' } }" active-class='active disabled'>Entertaiment</b-nav-item>
+                <b-nav-item :to="{ name: 'Tag',params: { tagname: 'sports' } }" active-class='active disabled'>Sports</b-nav-item>
+                <b-nav-item :to="{ name: 'Tag',params: { tagname: 'lifestyle' } }" active-class='active disabled'>Lifestyle</b-nav-item>
+                <b-nav-item :to="{ name:'About' } " active-class="active disabled">About</b-nav-item>
+                <b-nav-item :to="{ name: 'CreateArtikel' }" active-class='active disabled'>Insert</b-nav-item>
+            </b-navbar-nav>
 
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto" v-if="isLogged === false">
-            <b-nav-item :to="{ name: 'Login' }" active-class="active disabled">Login</b-nav-item>
-            <b-nav-item class="disabled">/</b-nav-item>
-            <b-nav-item :to="{ name: 'Register' }" active-class="active disabled">Register</b-nav-item>
-            <!-- <b-nav-item to="login" v-if="isLogged === false">Login</b-nav-item> -->
-        </b-navbar-nav >
-        <b-navbar-nav v-if="isLogged === true" class="ml-auto"> 
-            <b-nav-item :to="{ name: 'CreateArtikel' }" active-class='active disabled'>Create article</b-nav-item>    
-            <!-- Modal Component -->
-            <b-nav-item-dropdown right >
-            <!-- Using 'button-content' slot -->
-                <template slot="button-content"><em>{{ UserData.name }}</em></template>
-                <b-dropdown-item href="#"  >Profile</b-dropdown-item>
-                <b-dropdown-item @click.prevent="signout">Sign Out</b-dropdown-item>
-            </b-nav-item-dropdown>
-        </b-navbar-nav>
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item v-if="isLogged === false" :to="{ name: 'Login' }" active-class="active disabled">Login</b-nav-item>
+                <b-nav-item v-if="isLogged === false" :to="{ name: 'Register' }" active-class="active disabled">Register</b-nav-item>
+
+                <b-nav-item v-if="isLogged === true" :to="{ name: 'CreateArtikel' }" active-class='active disabled'>Create article</b-nav-item>    
+                <!-- Modal Component -->
+                <b-nav-item-dropdown v-if="isLogged === true" right >
+                <!-- Using 'button-content' slot -->
+                    <template v-if="isLogged === true" slot="button-content"><em>{{ UserData.name }}</em></template>
+                    <b-dropdown-item v-if="isLogged === true" href="#"  >Profile</b-dropdown-item>
+                    <b-dropdown-item v-if="isLogged === true" @click.prevent="signout">Sign Out</b-dropdown-item>
+                </b-nav-item-dropdown>
+            </b-navbar-nav>            
         </b-collapse>
     </b-navbar>
     </div>
@@ -180,6 +177,13 @@ export default {
 <style scoped>
 .navbars{
     background-color: darkkhaki;
+}
+.menus{
+    position: absolute;
+    width: 100%;
+    left: 0;
+    text-align: center;
+    
 }
 </style>
 
